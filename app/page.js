9cff15ea2d4e2c39ccc3bff6c48cfdbeade4e79c
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2024-11-06 00:32:36 -0300

    Detection with inferencejs

--- app/page.js
+++ app/page.js
@@ -1,101 +1,134 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { InferenceEngine, CVImage } from "inferencejs";
+import { useEffect, useRef, useState, useMemo } from "react";
+
+export default function App() {
+  const inferEngine = useMemo(() => {
+    return new InferenceEngine();
+  }, []);
+
+  const [modelWorkerId, setModelWorkerId] = useState(null);
+  const [modelLoading, setModelLoading] = useState(false);
+
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    if (!modelLoading) {
+      setModelLoading(true);
+      inferEngine
+        .startWorker("azul-board-game", 3, process.env.NEXT_PUBLIC_ROBOFLOW_API_KEY)
+        .then((id) => setModelWorkerId(id));
+    }
+  }, [inferEngine, modelLoading]);
+
+  useEffect(() => {
+    console.log("Model Worker ID: " + modelWorkerId);
+    if (modelWorkerId) {
+      startWebcam();
+    }
+  }, [modelWorkerId]);
+
+  const startWebcam = () => {
+    var constraints = {
+      audio: false,
+      video: {
+        width: { ideal: 640 },
+        height: { ideal: 480 },
+        facingMode: "environment",
+      },
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      videoRef.current.srcObject = stream;
+      videoRef.current.onloadedmetadata = () => {
+        videoRef.current.play();
+      };
+
+      videoRef.current.onplay = () => {
+        var ctx = canvasRef.current.getContext("2d");
+
+        var height = videoRef.current.videoHeight;
+        var width = videoRef.current.videoWidth;
+
+        videoRef.current.width = width;
+        videoRef.current.height = height;
+
+        canvasRef.current.width = width;
+        canvasRef.current.height = height;
+
+        ctx.scale(1, 1);
+
+        detectFrame();
+      };
+    });
+  };
+
+  const detectFrame = () => {
+    if (!modelWorkerId) setTimeout(detectFrame, 100 / 3);
+
+    const img = new CVImage(videoRef.current);
+    inferEngine.infer(modelWorkerId, img).then((predictions) => {
+      var ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+      for (var i = 0; i < predictions.length; i++) {
+        console.log(predictions[i]);
+        var prediction = predictions[i];
+
+        // draw detections
+        ctx.strokeStyle = prediction.color;
+
+        var x = prediction.bbox.x - prediction.bbox.width / 2;
+        var y = prediction.bbox.y - prediction.bbox.height / 2;
+        var width = prediction.bbox.width;
+        var height = prediction.bbox.height;
+
+        ctx.rect(x, y, width, height);
+        ctx.fillStyle = "rgba(0, 0, 0, 0)";
+        ctx.fill();
+        ctx.fillStyle = ctx.strokeStyle;
+        ctx.lineWidth = "4";
+        ctx.strokeRect(x, y, width, height);
+
+        var text = ctx.measureText(
+          prediction.class + " " + Math.round(prediction.confidence * 100) + "%"
+        );
+        ctx.fillStyle = ctx.strokeStyle;
+        ctx.fillRect(x - 2, y - 30, text.width + 4, 30);
+        ctx.font = "15px monospace";
+        ctx.fillStyle = "black";
+        ctx.fillText(
+          prediction.class +
+            " " +
+            Math.round(prediction.confidence * 100) +
+            "%",
+          x,
+          y - 10
+        );
+      }
+
+      setTimeout(detectFrame, 100 / 3);
+    });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div>
+      <div className="relative">
+        <video
+          width="640"
+          height="480"
+          ref={videoRef}
+          className="relative"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <canvas
+          width="640"
+          height="480"
+          ref={canvasRef}
+          className="absolute top-0 left-0"
+        />
+      </div>
     </div>
   );
 }
